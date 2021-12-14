8d8439b1db9094a9ffdad9d07c52a976e6f49651
Steve Testa <steve@testasoftware.com>
2021-12-14 09:31:54 -0500

    Svelte Template Project
    - SvelteKit
    - TailwindCSS

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Alert from '../components/alert.svelte'
+</script>
+
+<Alert>component example</Alert>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
